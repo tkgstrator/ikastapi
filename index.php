789dbe2f6b0b8dba01743d41b2772affa285b522
tkgstrator <nasawake.am@gmail.com>
2021-03-17 20:46:52 +0900

    - TwitterOAuthと偽のキー追加

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-    echo("Hello, world!");
+require "vendor/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+ 
+session_start();
+define("TWITTER_API_KEY", "htk6P4lNwXg1v6XUiQa0VsaJm");
+define("TWITTER_API_SECRET", TWITTER_API_SECRET);
+define("CALLBACK_URL", "TDOPciJ9WkM5SSxS2kSOfHSLdaQYyGkeH0D61OVgvq2YwFJF31");
+ 
+$connection = new TwitterOAuth(TWITTER_API_KEY, TWITTER_API_SECRET);
+$request = $connection->oauth("oauth/request_token", array("oauth_callback" => CALLBACK_URL));
+  
+$_SESSION["oauth_token"] = $request["oauth_token"];
+$_SESSION["oauth_token_secret"] = $request["oauth_token_secret"];
+  
+$url = $connection->url("oauth/authorize", array("oauth_token" => $request["oauth_token"]));
+header("Location: ".$url);
+exit;
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a981e09315072c9d7528ebe0c886abfa39fe6bd9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a981e09315072c9d7528ebe0c886abfa39fe6bd9',
+    ),
+    'abraham/twitteroauth' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af6d0ba772731d4f83524fccb24281fe6149ef43',
+    ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.2.9',
+      'version' => '1.2.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '78a0e288fdcebf92aa2318a8d3656168da6ac1a5',
+    ),
+  ),
+);
